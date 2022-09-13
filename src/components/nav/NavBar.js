18c760feb0f626b1ce9c1feaf4b1e6694e0c5e88
Jeremy Myers <echelonical@gmail.com>
2022-09-13 15:27:39 -0500

    Routing for TaskList works to render basic html <h2>tagged<h2> element.  Minor updates to Login to display userEmail Updates to ApplicationViews

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,14 +7,14 @@ export const NavBar = () => {
     return (
         <ul className="navbar">
             <li className="navbar__item active">
-                <Link className="navbar__link" to="/tickets">Tickets</Link>
+                <Link className="navbar__link" to="/tasks">Tasks</Link>
             </li>
             {
-                localStorage.getItem("honey_user")
+                localStorage.getItem("done_user")
                     ? <li className="navbar__item navbar__logout">
                         <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("honey_user")
-                            navigate("/", {replace: true})
+                            localStorage.removeItem("done__user")
+                            navigate("/", { replace: true })
                         }}>Logout</Link>
                     </li>
                     : ""
