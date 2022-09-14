5ef8533501f7ac5643e51d70d9f3f23ed83a09d4
Jeremy Myers <echelonical@gmail.com>
2022-09-14 15:50:29 -0500

    Link to see descrition/instructions functional

--- src/components/tasks/TaskDetails.js
+++ src/components/tasks/TaskDetails.js
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 export const TaskDetails = () => {
+
+    const { taskId } = useParams()
+    const [assignments, updateAssignments] = useState({
+        task: "",
+
+    })
+
+
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/assignments?_expand=user&_expand=task&taskId=${taskId}`)
+                .then(response => response.json())
+                .then((data) => {
+                    const singleTask = data[0]
+                    updateAssignments(singleTask)
+                })
+        },
+        [taskId]
+    )
 
     //This module will display instruction and/or descriptions of the task assigned. 
     //It will link to the current task link clicked to display the task details as mentioned above...
@@ -6,11 +28,23 @@ export const TaskDetails = () => {
 
 
     return <>
-        <h2>Task Details</h2>
-        <h4>Instructions</h4>
-        <p>Rinse dishes after scrapping food and debris.  Then load dishwasher.  Use new pouches under the sink.  Be sure to wash my lunch bowl and Yeti cup.</p>
-        <button>Return to Task List</button>
-        <button>Done</button>
-
+        <section>
+            <h2>Task Details</h2>
+            <h4>Instructions</h4>
+            <div>{assignments.task.instructions} </div>
+            <button>Return to Task List</button>
+            <button>Done</button>
+        </section>
     </>
 }
+
+/* <section className="tasks">
+    <header className="task__header">{task?.user?.fullName}</header>
+
+    <div>Email: {employee?.user?.email}</div>
+    <div>Specialty: {employee.specialty}</div>
+    <div>Rate: {employee.rate}</div>
+
+    <footer className="employee__footer">Currently working on {employee?.employeeTickets?.length} tickets</footer>
+
+</section> */
