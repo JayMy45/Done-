41e884c42412f65e7fbb3dfbbe2582494d869b94
Jeremy Myers <echelonical@gmail.com>
2022-09-14 12:01:52 -0500

    added styling to task list link.  Created template code to distplay TaskDetails

--- src/components/tasks/TaskDetails.js
+++ src/components/tasks/TaskDetails.js
@@ -1,3 +1,16 @@
 export const TaskDetails = () => {
-    return <h2>Hellow Detailedie Worldie</h2>
+
+    //This module will display instruction and/or descriptions of the task assigned. 
+    //It will link to the current task link clicked to display the task details as mentioned above...
+
+
+
+    return <>
+        <h2>Task Details</h2>
+        <h4>Instructions</h4>
+        <p>Rinse dishes after scrapping food and debris.  Then load dishwasher.  Use new pouches under the sink.  Be sure to wash my lunch bowl and Yeti cup.</p>
+        <button>Return to Task List</button>
+        <button>Done</button>
+
+    </>
 }
